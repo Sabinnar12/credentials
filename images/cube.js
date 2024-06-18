8843ca69856688e8a547d3b071e1e46be3498f34
Sabinnar12 <41190010+Sabinnar12@users.noreply.github.com>
2024-06-18 15:27:35 +0545

    Update cube.js

--- images/cube.js
+++ images/cube.js
@@ -1,1 +1,9 @@
 
+  document.addEventListener('DOMContentLoaded', function() {
+    const closeBtn = document.querySelector('.close-btn');
+    const cubeContainer = document.querySelector('.cube-container');
+
+    closeBtn.addEventListener('click', function() {
+        cubeContainer.style.display = 'none';
+    });
+});
