54ad1491d4b0299ad710a78225d469a1c8506520
Sabinnar12 <41190010+Sabinnar12@users.noreply.github.com>
2024-06-18 15:50:28 +0545

    Update and rename cube.js to cubejs.js

--- images/cubejs.js
+++ images/cubejs.js
@@ -1,9 +1,13 @@
-
-  document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function() {
     const closeBtn = document.querySelector('.close-btn');
     const cubeContainer = document.querySelector('.cube-container');
+    const cube = document.querySelector('.cube');
 
     closeBtn.addEventListener('click', function() {
         cubeContainer.style.display = 'none';
+    });
+
+    cube.addEventListener('click', function() {
+        window.open('https://www.sabinrokka.com.np/payment', '_blank');
     });
 });
